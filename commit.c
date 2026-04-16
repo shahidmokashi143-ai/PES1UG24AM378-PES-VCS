4579697589cb92d61271e0503dbb390e53338848
Mohammed Shahid <shahidmokashi143@gmail.com>
2026-04-16 05:53:38 +0000

    Phase 4: Implement commit system (manual tree build)

--- commit.c
+++ commit.c
@@ -194,8 +194,76 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Index index;
+
+    // 1. Load index
+    if (index_load(&index) != 0) return -1;
+
+    // 2. Build flat tree manually
+    Tree tree;
+    tree.count = 0;
+
+    for (int i = 0; i < index.count; i++) {
+        TreeEntry *te = &tree.entries[tree.count++];
+
+        te->mode = index.entries[i].mode;
+        strcpy(te->name, index.entries[i].path);
+        te->hash = index.entries[i].hash;
+    }
+
+    // 3. Serialize tree
+    void *tree_data;
+    size_t tree_len;
+
+    if (tree_serialize(&tree, &tree_data, &tree_len) != 0)
+        return -1;
+
+    ObjectID tree_id;
+    if (object_write(OBJ_TREE, tree_data, tree_len, &tree_id) != 0) {
+        free(tree_data);
+        return -1;
+    }
+
+    free(tree_data);
+
+    // 4. Prepare commit struct
+    Commit commit;
+    memset(&commit, 0, sizeof(Commit));
+
+    commit.tree = tree_id;
+
+    // 5. Parent
+    if (head_read(&commit.parent) == 0) {
+        commit.has_parent = 1;
+    } else {
+        commit.has_parent = 0;
+    }
+
+    // 6. Author + time
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+
+    // 7. Message
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    // 8. Serialize commit
+    void *data;
+    size_t len;
+
+    if (commit_serialize(&commit, &data, &len) != 0)
+        return -1;
+
+    // 9. Store commit
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    // 10. Update HEAD
+    if (head_update(commit_id_out) != 0)
+        return -1;
+
+    return 0;
 }
